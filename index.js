8ecb2575601c52f1f91a4d3729a17587e00a1e5d
Holly <hollykvalheim@gmail.com>
2022-10-31 18:50:40 -0700

    commit tree 3b5957d2e81a311f74b65ea93d537c0c20745c7c

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+function findMatching(array, string){
+    return array.filter(function(name){
+        return name.toLowerCase() === string.toLowerCase()
+    })
+}
+
+function fuzzyMatch(array, string){
+    return array.filter(function(name){
+        const len = string.length
+        return name.slice(0,len) === string
+    })
+
+}
+
+function matchName(array, string){
+    return array.filter(function(element){
+        return element.name === string
+})
+}
